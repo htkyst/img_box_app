217604a72f0267a4f03b12a2ce3df13862bdb52b
htkyst <rubybox@outlook.jp>
2021-09-14 22:47:17 +0900

    add deco

--- user_info.py
+++ user_info.py
@@ -4,3 +4,11 @@ USER_LIST = {}
 
 def is_login():
     return 'login' in session
+
+def RequireLogin(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not is_login():
+            return redirect('login')
+        return func(*args, **kwargs)
+    return wrapper
